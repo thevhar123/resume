fcf5de60a0f8874636a367c1d65cf5e4fda730e5
thevhar123 <thevaharb23aid@vetias.ac.in>
2025-05-14 09:49:52 +0530

    Add files via upload

---
+++ c_demo.c
@@ -0,0 +1,15 @@
+//online c compiler to run c program online
+#include <stdio.h>
+
+int main() 
+{
+       int k;
+       k=6;
+       int a=4;
+       float b=2.07899;
+       char c='c';
+       printf("%f",b);
+       printf("%c",c);
+       printf("%d",a);
+       return 0;
+}
